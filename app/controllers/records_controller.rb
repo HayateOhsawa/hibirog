08b6afae5e6eb6bed500ba941741d154e6483533
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-13 21:43:14 +0900

    新規投稿画面の実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,29 @@
 class RecordsController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @records = current_user.records.order(created_at: :desc)
+  end
+
+  def new
+    @record = Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+    @record.user_id = current_user.id # current_userからuser_idを設定
+    if @record.save
+      # 成功した場合、indexページへリダイレクト
+      redirect_to root_path
+    else
+      # 保存に失敗した場合は再度フォームを表示
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # ストロングパラメータの設定
+  def record_params
+    params.require(:record).permit(:title, :description, :emotion, :location, :retention_level_id, :file)
   end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,15 @@ class Record < ApplicationRecord
   # has_many :comments
   # has_one :chat
   belongs_to :user
+
+  with_options presence: true do
+    validates :title, length: { maximum: 100 }
+    validates :description
+    validates :retention_level_id
+  end
+
+  # 他のフィールドのバリデーション設定
+  validates :title, length: { maximum: 100 }
+  validates :emotion, length: { maximum: 100 }
+  validates :location, length: { maximum: 100 }
 end
