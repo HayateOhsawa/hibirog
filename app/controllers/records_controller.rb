95db24aa5f6c1895369c5a41488a8ff065a06acb
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-14 14:43:19 +0900

    コントローラーの設定

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,6 @@
 class RecordsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_record, only: [:show, :edit, :update] # , :destroy
+  before_action :set_record, only: [:show, :edit, :update, :destroy]
   def index
     @records = current_user.records.order(created_at: :desc)
   end
