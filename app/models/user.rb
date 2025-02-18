a8ffe6786d41ab9f1c75f07bb3ba5223ba1a48e4
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-18 16:10:03 +0900

    テストコードをコピペ

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   has_many :records
   has_many :chats
   has_many :comments, dependent: :destroy
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/i # 6文字以上
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて6文字以上で設定してください' }, if: -> { password.present? }
+    validates :birth_date
+  end
+
+  # occupation_idが1だと登録できない
+  validates :occupation_id, numericality: { other_than: 1 }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { 'テストユーザー' }
+    email { 'test@example.com' }
+    password { 'password123' }
+    password_confirmation { 'password123' }
+    birth_date { '2000-01-01' }
+    occupation_id { 2 } # 1以外のoccupation_idを設定
   end
 end
