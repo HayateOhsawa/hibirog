d346af7adaeb8db2f73d3ddf7a67692aa88d36d8
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-18 13:15:15 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "records#index"
-  resources :records
+  resources :records do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :chats, only: [:index, :create, :destroy]
   resources :records do
     post :share, to: 'records#share', on: :member # on: :member でレコードIDがURLに含まれる
