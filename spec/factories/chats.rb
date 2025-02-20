81e8fcf83f4f980c7af24dceee12ad391cf235ee
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-20 17:44:04 +0900

    チャットのテストコード

--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :chat do
-    message { 'テストメッセージ' } # メッセージの例
+    message_content { 'テストメッセージ' } # メッセージの例
     association :user # Userが必要なら、FactoryBotを使用してユーザーを関連付ける
   end
 end
