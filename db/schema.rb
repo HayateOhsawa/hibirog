dd78a6443936c7fa76791a583e0e3ae0a1cccc75
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-21 18:20:32 +0900

    タグのマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_20_095123) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_21_091739) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -69,6 +69,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_20_095123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_records_on_user_id"
+  end
+
+  create_table "tags", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
