639bd59388c3655b708f6e391e0dd0bec7aff6df
HayateOhsawa <hayateoosawa3150@gmail.com>
2025-02-25 15:48:15 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root "records#index"
   resources :records do
     resources :comments, only: [:create, :destroy]
+<<<<<<< Updated upstream
+=======
+    post :share, to: 'records#share', on: :member # on: :member でレコードIDがURLに含まれる
+    post :add_tag, to: 'records#add_tag', on: :member
+>>>>>>> Stashed changes
   end
   resources :chats, only: [:index, :create, :destroy]
   resources :records do
